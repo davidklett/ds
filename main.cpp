0711c63a216324f15373fb101e9469efd8d55637
David Klett <dgk10@zips.uakron.edu>
2019-02-25 12:47:51 -0500

    Completed find_gt function

--- main.cpp
+++ main.cpp
@@ -19,12 +19,13 @@ list<int>::iterator find_gt(list<int>::iterator start, list<int>::iterator stop,
 
 int main()
 {
+
     vector<list<int>> adjList;
     list<int> mtLst;  // an empty list
 
-  string line;
-  ifstream myfile ("graph.dat");
-  int countV = 0;
+      string line;
+      ifstream myfile ("graph.dat");
+      int countV = 0;
 
       if (myfile.is_open())
       {
@@ -37,7 +38,10 @@ int main()
                 int x;
                 //populate the linked-list!
                 while( is >> x ) {
-                      adjList[countV].push_back(x); //use insert and find_gt here?
+                    std::list<int>::iterator gt_iter = find_gt(adjList[countV].begin(),adjList[countV].end(), x);
+                    //adjList[countV].push_back(x); //use insert and find_gt here?
+                    //list<int>::insert(gt_iter, x);
+                    adjList[countV].insert(gt_iter, x);
                 }
 
           countV++;
@@ -63,7 +67,7 @@ int main()
     adjListFinal.push_back(myList);
     std::list<int>::iterator temp;
     temp = adjList[0].begin();
-    list::insert(temp, adjList[0].begin()); //not working!!!
+    //list::insert(temp, adjList[0].begin()); //not working!!!
 
     return 0;
 }
@@ -72,12 +76,15 @@ list<int>::iterator find_gt(list<int>::iterator start, list<int>::iterator stop,
 {
 
   std::list<int>::iterator temp_iter;
-  /*
+
   for(temp_iter = start; temp_iter != stop; ++temp_iter)
   {
         if(*temp_iter > x)
             return temp_iter;
-  }*/
+  }
+
+  return stop;
+  /*
 
   temp_iter = (++start);
   if(*temp_iter > *start)
@@ -90,5 +97,5 @@ list<int>::iterator find_gt(list<int>::iterator start, list<int>::iterator stop,
       *temp_iter = *start;
       *start = *temp_iter;
       return temp_iter;
-  }
+  }*/
 }
